cb03b3bbe57259b39936489fd7c3ee3a4f5f05dc
christianzaboli <zabolichristian@gmail.com>
2025-09-02 13:19:31 +0200

    inizio sezione palindroma; immissione parola utente

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,4 +1,8 @@
 // Palidroma
 
+
+
 // Chiedere all’utente di inserire una parola
+const userWord = prompt("Inserisci una parola e scopri se é palindroma")
+
 // Creare una funzione per capire se la parola inserita è palindroma
