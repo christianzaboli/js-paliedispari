f85988181bf2e1e14e6ca8c2d15668cfaec86dfb
christianzaboli <zabolichristian@gmail.com>
2025-09-02 14:25:50 +0200

    funzione palindromeTest finita

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -3,22 +3,30 @@
 
 
 // Chiedere all’utente di inserire una parola
-const userWord = prompt("Inserisci una parola e scopri se é palindroma")
-const userWordReverted = palindromeTest(userWord)
-console.log("Parola scelta dall'utente:", userWordReverted);
+const userWord = prompt("Inserisci una parola e scopri se é palindroma");
+console.log(userWord);
+
+const palindromeUserWord = palindromeTest(userWord);
+console.log("Parola scelta dall'utente é palindroma?", palindromeUserWord);
 
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // Funzione di revert della parola
 function palindromeTest(word) {
-    let revertedWord = ""
-    let verification = false
+    
+    //  VARIABILI DI SERVIZIO PER LA SVOLGIMENTO DELLA FUNZIONE
+    let revertedWord = "";
+    let verification = false;
 
     for (let i = word.length - 1; i >= 0; i--) {
         let letterIndexes = word[i];
+        revertedWord += letterIndexes
+    };
+    console.log(revertedWord);   // LOG DELLA PAROLA DELL'UTENTE REVERTED
+    
+    if (revertedWord.toUpperCase() === word.toUpperCase()) {
+        verification = true
+    };          // COMPARAZIONE DELLA PAROLA REVERTATA CON LA PAROLA SCELTA PER LA VERIFICA
 
-        revertedWord = revertedWord + letterIndexes
-        
-    }
-    return revertedWord
+    return verification;        // CHIUSURA FUNZIONE
 }
