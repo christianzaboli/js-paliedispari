adebeeb761e6ab50df1a8aeb4ba618501cbef022
christianzaboli <zabolichristian@gmail.com>
2025-09-02 14:45:07 +0200

    inizio dell'esercizio Pari e Dispari

--- Pari_e_Dispari/script.js
+++ Pari_e_Dispari/script.js
@@ -5,3 +5,19 @@
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
+// L’utente sceglie pari o dispari.
+const userChoice = prompt("Scegli tra pari o dispari")
+console.log(userChoice);
+
+// L’utente sceglie un numero da 1 a 5
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+// Sommiamo i due numeri
+
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+
+// Dichiariamo chi ha vinto.
